2d85b0285d9b943b8c5b92b1f25117344c1fd066
Sampath1574 <121035133+Sampath1574@users.noreply.github.com>
2024-03-10 18:17:57 +0530

    Time: 59 ms (38.31%) | Memory: 51.5 MB (47.47%) - LeetSync

---
+++ 2798-chunk-array/chunk-array.js
@@ -0,0 +1,8 @@
+var chunk = function(arr, size) {
+    if(arr.length == 0) return [];
+    
+    const result = [arr.slice(0, size)];
+    result.push(...chunk(arr.slice(size), size)) // spread operator to avoid nested sub-arrays
+    
+    return result;
+};
